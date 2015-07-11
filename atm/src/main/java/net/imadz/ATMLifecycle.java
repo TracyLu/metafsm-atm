b46ee81e32e2c30019c63cf42deb62d938ce9185
TracyLu <hailinglu@126.com>
2015-07-11 16:26:14 +0800

    Add state machine definition for atm.

--- atm/src/main/java/net/imadz/ATMLifecycle.java
+++ atm/src/main/java/net/imadz/ATMLifecycle.java
@@ -1,7 +1,76 @@
 package net.imadz;
 
-/**
- * Created by Scala on 15-7-8.
- */
+import net.imadz.lifecycle.annotations.*;
+import net.imadz.lifecycle.annotations.action.ConditionSet;
+import net.imadz.lifecycle.annotations.action.Conditional;
+import net.imadz.lifecycle.annotations.action.ConditionalEvent;
+import net.imadz.lifecycle.annotations.state.Final;
+import net.imadz.lifecycle.annotations.state.Initial;
+
+@StateMachine
 public interface ATMLifecycle {
+
+    @StateSet
+    interface States {
+        @Initial
+        @Transition(event = Events.Deposit.class, value = NonEmpty.class)
+        class Empty {
+        }
+
+        @Transitions({@Transition(event = Events.Deposit.class, value = {NonEmpty.class, Full.class}),
+                @Transition(event = Events.Withdraw.class, value = {NonEmpty.class, Empty.class}),
+                @Transition(event = Events.Stop.class, value = Stopped.class)
+        })
+        class NonEmpty {
+        }
+
+        @Transitions({@Transition(event = Events.Withdraw.class, value = NonEmpty.class),
+                @Transition(event = Events.Stop.class, value = Stopped.class)
+        })
+        class Full {
+        }
+
+        @Final
+        class Stopped {}
+
+    }
+
+    @EventSet
+    interface Events {
+
+        @Conditional(condition = Conditions.CashCounter.class, judger = Utilities.TotalCashJudger.class, postEval = true)
+        class Deposit {
+        }
+
+        @Conditional(condition = Conditions.CashCounter.class, judger = Utilities.TotalCashJudger.class, postEval = true)
+        class Withdraw {
+        }
+
+        class Stop {}
+    }
+
+    @ConditionSet
+     interface Conditions {
+
+        interface CashCounter {
+
+            Integer getTotalCash();
+        }
+    }
+
+    class Utilities {
+
+        public static class TotalCashJudger implements ConditionalEvent<Conditions.CashCounter> {
+            @Override
+            public Class<?> doConditionJudge(Conditions.CashCounter t) {
+                if (t.getTotalCash() >= 1000) {
+                    return States.Full.class;
+                } else if (t.getTotalCash() < 1000 && t.getTotalCash() > 0) {
+                    return States.NonEmpty.class;
+                } else {
+                    return States.Empty.class;
+                }
+            }
+        }
+    }
 }
--- atm/src/main/scala/net/imadz/ATM.scala
+++ atm/src/main/scala/net/imadz/ATM.scala
@@ -1,8 +1,41 @@
 package net.imadz
 
+import net.imadz.ATMLifecycle.Events.{Stop, Withdraw, Deposit}
+import net.imadz.lifecycle.annotations.action.Condition
+import net.imadz.lifecycle.annotations.{Event, StateIndicator, LifecycleMeta}
+
 /**
  * Created by Scala on 15-7-8.
  */
-class ATM {
+@LifecycleMeta(classOf[ATMLifecycle])
+case class ATM() extends ATMLifecycle.Conditions.CashCounter{
+  private var totalCash: Int = 0
+
+  @StateIndicator
+  private var state = "Empty"
+
+  @Condition(classOf[ATMLifecycle.Conditions.CashCounter])
+  def getCashCounter() = this
+
+
+  def getTotalCash(): Integer = totalCash
+
+  def getState() = state
+
+  @Event(classOf[Deposit])
+  def deposit() = {
+    totalCash += 100
+  }
+
+
+  @Event(classOf[Withdraw])
+  def withdraw() = {
+    totalCash -= 100
+  }
+
+  @Event(classOf[Stop])
+  def stop() = {
+    totalCash = 0
+  }
 
 }
